51fc35e02857cd9ee2037a7fdd0364d5f954d633
Tharon Weighill <tharonweighill@gmail.com>
2020-07-31 21:33:11 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
+countdown(10)
 
-def countdown
+def countdown_with_sleep(number)
+  while number > 0
+   sleep 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+countdown(10)
